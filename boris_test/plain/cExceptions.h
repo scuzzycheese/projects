488042030a87eb07a4aabc0c72b990f9c08a1967
scuzzy <scuzzy@2057c996-5942-0410-8ad5-dfecb10f4c5e>
2010-03-14 09:26:43 +0000

    addind exceptions class
    git-svn-id: http://bitrandom.svnrepository.com/svn/homework@73 2057c996-5942-0410-8ad5-dfecb10f4c5e

---
+++ boris_test/plain/cExceptions.h
@@ -0,0 +1,23 @@
+#ifndef CEXCEPTIONS_H
+#define CEXCEPTIONS_H
+
+
+#include <iostream>
+#include <exception>
+
+class bad_capacity : public std::exception
+{
+	std::string mMessage;
+
+	public:
+	bad_capacity(const std::string& msg = "Capacity Exception") : mMessage(msg) {}
+	~bad_capacity() throw() {}
+
+	virtual const char* what() const throw()
+	{
+		return mMessage.c_str();
+	}
+};
+	
+#endif
+
