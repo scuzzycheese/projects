5a0ba4fa777cf75e7ee78d813ef638c52f283552
scuzzy <scuzzy@2057c996-5942-0410-8ad5-dfecb10f4c5e>
2011-05-19 20:58:34 +0000

    I'm on the right track
    git-svn-id: http://bitrandom.svnrepository.com/svn/homework@102 2057c996-5942-0410-8ad5-dfecb10f4c5e

--- aes/aes.c
+++ aes/aes.c
@@ -22,6 +22,7 @@ int main()
 	unsigned char *encoded = encode("abc00005d41402abc4b2a76b9719d911017c592", sizeof("abc00005d41402abc4b2a76b9719d911017c592"));
 	printf("ENCODED: %s\n", encoded);
 	printf("LENGTH: %u\n\n", strlen(encoded));
+
 	unsigned char *decoded = decode(encoded, strlen(encoded));
 	printf("DECODED: %s\n", decoded);
 	free(encoded);
@@ -54,10 +55,11 @@ char *encode(const unsigned char *input, int length)
 	BUF_MEM *bioBuff;
 
 	bioB64 = BIO_new(BIO_f_base64());
+	BIO_set_flags(bioB64, BIO_FLAGS_BASE64_NO_NL);
 	bioMem = BIO_new(BIO_s_mem());
 
 	bioCipher = BIO_new(BIO_f_cipher());
-	setCipher(bioCipher, "myAesKey", 8, CIPHER_ENCRYPT);
+	setCipher(bioCipher, "myAesKey12345678", 16, CIPHER_ENCRYPT);
 
 	//build the stack:
 	//bioCipher -> bioB64 -> bioMem
@@ -79,28 +81,32 @@ char *encode(const unsigned char *input, int length)
 
 char *decode(unsigned char *input, int length)
 {
+	printf("Input string: %s\n", input);
 	BIO *bioMem;
 	BIO *bioB64;
 	BIO *bioCipher;
-	BUF_MEM bioBuff;
 
 	bioB64 = BIO_new(BIO_f_base64());
+	BIO_set_flags(bioB64, BIO_FLAGS_BASE64_NO_NL);
 	bioMem = BIO_new_mem_buf(input, length);
 
-	//bioCipher = BIO_new(BIO_f_cipher());
-	//setCipher(bioCipher, "myAesKey", 8, CIPHER_DECRYPT);
+	bioCipher = BIO_new(BIO_f_cipher());
+	setCipher(bioCipher, "myAesKey12345678", 16, CIPHER_DECRYPT);
 
 	bioB64 = BIO_push(bioB64, bioMem);
+	bioCipher = BIO_push(bioCipher, bioB64);
 
 	char *outBuff = (char *)malloc(length);
-	int readBytes = BIO_read(bioB64, outBuff, length);
-	ERR_print_errors(NULL);
+	memset(outBuff, 0x00, length);
+	int readBytes = BIO_read(bioCipher, outBuff, length);
+	ERR_print_errors(bioMem);
+	ERR_print_errors(bioB64);
 	printf("Number of bytes read: %d\n", readBytes);
 	outBuff[readBytes] = '\0';
 
 
 	BIO_free_all(bioCipher);
 	return outBuff;
-
-
 }
+
+
